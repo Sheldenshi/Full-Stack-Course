2bc305cb19cccce8c81fd7bea72f92884ef478bc
Shelden <shelden@berkeley.edu>
2021-01-04 20:45:05 -0800

    JSX

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,11 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  console.log('Hello from component')
-  return (
-    <div>
-      <p>Hello world</p>
-    </div>
+  //console.log('Hello from component')
+  const now = new Date()
+  const a = 10
+  const b = 20
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'p', null, 'It is ', now.toString()
+    ),
+    React.createElement(
+      'p', null, a, 'plus', b, 'is', a + b
+    )
   )
 }
 
