6a6fdfa684742316392e4c1fedc9f96ab51886e9
Shelden <shelden@berkeley.edu>
2021-01-04 21:12:44 -0800

    multiple components

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+/*
 const App = () => {
   //console.log('Hello from component')
   const now = new Date()
@@ -15,6 +16,24 @@ const App = () => {
     React.createElement(
       'p', null, a, 'plus', b, 'is', a + b
     )
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+*/
+
+const Hello = () => {  
+  return (
+      <div>      
+        <p>Hello world</p>    
+      </div>  )}
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello /> 
+      <Hello />   
+    </div>
   )
 }
 
