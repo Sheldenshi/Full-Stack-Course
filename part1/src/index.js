881ebb157c05089baff645a0335fa06ffbc3a347
Shelden <shelden@berkeley.edu>
2021-01-04 22:03:16 -0800

    exercises 1.1-1.2

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,40 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-/*
 const App = () => {
-  //console.log('Hello from component')
-  const now = new Date()
-  const a = 10
-  const b = 20
-  return React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'p', null, 'It is ', now.toString()
-    ),
-    React.createElement(
-      'p', null, a, 'plus', b, 'is', a + b
-    )
-  )
-}
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
-ReactDOM.render(<App />, document.getElementById('root'))
-*/
-
-const Hello = (props) => {  
-  return (
-      <div>      
-        <p>Hello {props.name}, you are {props.age} years old</p>    
-      </div>  )}
-const App = () => {
-  const name = "Shelden"
-  const age = 21
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name = {name} age = {age}/> 
-      <Hello name = "Kat" age = {17 + 2}/>   
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
