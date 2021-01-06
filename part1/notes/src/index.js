656689bc850e5a09f9cacd8fcc9abaf6156a6eae
Shelden <shelden@berkeley.edu>
2021-01-05 18:55:12 -0800

    stateful component

--- part1/notes/src/index.js
+++ part1/notes/src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/*
+const Hello = ({name, age}) => {
+  const bornYear = () => new Date().getFullYear() - age
+  
+  return (
+    <div>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>
+        So you are probably born in {bornYear()}
+      </p>
+    </div>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
+*/
+const App = ({counter}) => {
+  return (
+    <div>{counter}</div>
+  )
+}
+
+let counter = 1
+
+const refresh = () => {
+  ReactDOM.render(<App counter={counter}/>,
+  document.getElementById('root'))
+}
+setInterval(() => {
+  refresh()
+  counter += 1
+}, 1000)
+
