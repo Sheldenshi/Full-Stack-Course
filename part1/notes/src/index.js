194ec65b8128c7983f2d38f68fd60de21d67de7d
Shelden <shelden@berkeley.edu>
2021-01-07 20:44:35 -0800

    debug

--- part1/notes/src/index.js
+++ part1/notes/src/index.js
@@ -75,6 +75,27 @@ setInterval(() => {
   counter += 1
 }, 1000)
 */
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  } else {
+    return (
+      <div>
+      button press history: {props.allClicks.join(' ')}
+      </div>
+    )
+  }
+}
+
+const Button = ({onClick, text}) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
 
 const App = () => {
   const [left, setLeft] = useState(0)
@@ -89,10 +110,11 @@ const App = () => {
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
+      <Button onClick={handleLeftClick} text='left' />      
+      <Button onClick={handleRightClick} text='right' />
       {right}
-      <p>{allClicks.join(' ')}</p>    </div>
+      <History allClicks = {allClicks}/> 
+    </div>
   )
 }
 ReactDOM.render(
