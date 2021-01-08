fc0faff289e9ab201ef5916302c691c687528e08
Shelden <shelden@berkeley.edu>
2021-01-07 20:34:52 -0800

    handling arrays

--- part1/notes/src/index.js
+++ part1/notes/src/index.js
@@ -29,27 +29,39 @@ const App = () => {
     </div>
   )
 }
-*/
+
+
+
+
+const Display = ({counter}) => {
+  return (
+    <div>{counter}</div>
+  )
+}
+const Button = ({handleClick, text}) => {
+  return (
+    <button onClick={handleClick}>
+        {text}
+    </button>
+  )
+  
+}
+
 const App = () => {
   const [counter, setCounter] = useState(0)
+  const plusOne = () => setCounter(counter + 1)
+  const minusOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>
-        plus
-      </button>
-      <button onClick={() => setCounter(0)}>
-        zero
-      </button>
+      <Display counter = {counter} />
+      <Button handleClick = {plusOne} text = 'PLUS'/>
+      <Button handleClick = {minusOne} text = 'MINUS'/>
+      <Button handleClick = {setToZero} text = 'ZERO'/>
     </div>
     
   )
 }
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
 
 /*
 let counter = 1
@@ -64,3 +76,26 @@ setInterval(() => {
 }, 1000)
 */
 
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const handleLeftClick = () => {    
+    setAll(allClicks.concat('L'))    
+    setLeft(left + 1)  }
+  const handleRightClick = () => {    
+    setAll(allClicks.concat('R'))    
+    setRight(right + 1)  }
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>    </div>
+  )
+}
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
