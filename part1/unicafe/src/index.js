688996558f381fcceb3270b4c98e5e7e71c88a17
Shelden <shelden@berkeley.edu>
2021-01-07 22:31:36 -0800

    1.11

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -25,24 +25,57 @@ const Paragraph = ({text, value}) => {
   )
 } 
 
+const Statistics = ({name, value, ending}) => {
+  return (
+    <tbody>
+      <tr>
+      <td>{name}</td>
+      <td>{value}</td>
+      <td>{ending}</td>
+    </tr>
+    </tbody>
+    
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  return (
-    <div>
-      <Display text="give feedback"/>
-      <Button handleClick={() => setGood(good + 1)} text = "good"/>
-      <Button handleClick={() => setNeutral(neutral + 1)} text = "neutral"/>
-      <Button handleClick={() => setBad(bad + 1)} text = "bad"/>
-      <Display text="statistics"/>
-      <Paragraph text = "good" value = {good}/>
-      <Paragraph text = "neutral" value = {neutral}/>
-      <Paragraph text = "bad" value = {bad}/>
-    </div>
-  )
+  if (good === 0 && neutral === 0 && bad ===0) {
+    return (
+      <div>
+        <Display text="give feedback"/>
+        <Button handleClick={() => setGood(good + 1)} text = "good"/>
+        <Button handleClick={() => setNeutral(neutral + 1)} text = "neutral"/>
+        <Button handleClick={() => setBad(bad + 1)} text = "bad"/>
+        <Paragraph text = "No feedback given"/>
+      </div>
+    )
+  } else {
+    return (
+    
+      <>
+        <Display text="give feedback"/>
+        <Button handleClick={() => setGood(good + 1)} text = "good"/>
+        <Button handleClick={() => setNeutral(neutral + 1)} text = "neutral"/>
+        <Button handleClick={() => setBad(bad + 1)} text = "bad"/>
+        <Display text="statistics"/>
+        <table>
+          <Statistics name = "good" value = {good}/>
+          <Statistics name = "neutral" value = {neutral}/>
+          <Statistics name = "bad" value = {bad}/>
+          <Statistics name = "all" value = {good + neutral + bad}/>
+          <Statistics name = "average" value = {(good - bad) / (good + neutral + bad)}/>
+          <Statistics name = "positive" value = {good / (good + neutral + bad)} ending = "%"/>
+        </table>
+        
+      </>
+    )
+  }
+  
 }
 
 ReactDOM.render(<App />, 
