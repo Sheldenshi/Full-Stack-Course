a81ae62b5c437b05fdf601fe966e534a42c95c94
Shelden <shelden@berkeley.edu>
2021-01-15 20:29:47 -0800

    extracting communication with the backend inot a separate module

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import Note from './Components/Note'
-import axios from 'axios'
+import noteService from './services/notes'
+
 
 const App = () => {
   
@@ -8,23 +9,22 @@ const App = () => {
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
+    noteService
+      .getAll()
       .then(response => {
-        console.log('promise fulfilled')
         setNotes(response.data)
       })
   }, [])
-  
+
   const toggleImportanceOf = id => {
-    const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
   
-    axios.put(url, changedNote).then(response => {
-      setNotes(notes.map(note => note.id !== id ? note : response.data))
-    })
+    noteService
+      .update(id, changedNote)
+      .then(response => {
+        setNotes(notes.map(note => note.id !== id ? note : response.data))
+      })
   }
   const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
   
@@ -37,8 +37,8 @@ const App = () => {
       important: Math.random() < 0.5
       
     }
-    axios
-      .post('http://localhost:3001/notes', noteObject)
+    noteService
+      .create(noteObject)
       .then(response => {
         setNotes(notes.concat(response.data))
         setNewNote('')
