94870ab0aa7f327d7f0407e275404770cadcfbe7
Shelden <shelden@berkeley.edu>
2021-01-15 19:32:17 -0800

    sending data to the server

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -20,13 +20,19 @@ const App = () => {
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
+      id: notes.length + 1,
       content: newNote,
       date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length + 1
+      important: Math.random() < 0.5
+      
     }
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      })
+    
   }
   const handleNoteChange = (event) => {
     console.log(event.target.value);
