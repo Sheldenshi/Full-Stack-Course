3d2aec4390bfe7c776dcae327a0ae04eb5b07a65
Shelden <shelden@berkeley.edu>
2021-03-21 21:44:38 -0700

    3.1 - 3.6

--- part3_ex/index.js
+++ part3_ex/index.js
@@ -25,8 +25,68 @@ app.get('/', (request, response) => {
     response.send('<h1 align=center>Phonebook</h1>')
 })
 
+app.get('/info', (request, response) => {
+    
+    response.send(`<h align=center>Phonebook has info for ${persons.length} people</h> </br>` + new Date())
+})
+
 app.get('/api/persons', (request, response) => {
     response.send(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+    
+
+    const body = request.body
+    
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name is missing'
+        })
+    } else if (!body.number) {
+        return response.status(400).json({
+            error: 'number is missing'
+        })
+    }
+    const names = persons.map((person) => person.name)
+    const uniqueName = !names.includes(body.name)
+
+    if (!uniqueName) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+    persons = persons.concat(person)
+    response.json(person)
 })
 
 const PORT = 3002
